ed06d8ac375f8d7298c9a66f81459d687b4db569
Victor Riurean <victor.riurean@gmail.com>
2024-08-28 23:22:57 +0200

    Implement CreateAutomaticallyView

---
+++ BookmarkProject/BookmarkProject/App/Tabs/NewResource/Views/CreateAutomaticallyView.swift
@@ -0,0 +1,135 @@
+//
+//  CreateAutomaticallyView.swift
+//  BookmarkProject
+//
+//  Created by Victor on 28/08/2024.
+//
+
+import DesignSystems
+import Models
+import SwiftUI
+
+
+@MainActor
+struct CreateAutomaticallyView: View {
+    
+    // MARK: Constants
+    
+    private enum Constants {
+        static let buttonHeight: CGFloat = 50.0
+        static let buttonWidth: CGFloat = 200.0
+    }
+    
+    
+    // MARK: Environment
+    
+    @Environment(Theme.self) var theme
+    
+    
+    // MARK: State properties
+    
+    @Binding var selectedTab: Tab
+    
+    @State private var asset: LearningAsset?
+    @State private var didFailToGenerate = false
+    @State private var isPrimaryButtonLoading = false
+    @State private var url = ""
+    
+    
+    // MARK: Public properties
+    
+    let generateAsset: (String) async throws -> LearningAsset
+    
+    
+    // MARK: Body
+    
+    var body: some View {
+        Group {
+            if let asset {
+                ConfirmAssetView(
+                    selectedTab: $selectedTab,
+                    parentAsset: $asset,
+                    asset: asset
+                )
+            } else {
+                generateAssetView
+            }
+        }
+        .alert(
+            "Failed to generate asset",
+            isPresented: $didFailToGenerate
+        ) {
+            Button("OK", role: .cancel) { }
+        }
+    }
+    
+    
+    // MARK: Subviews
+    
+    private var generateAssetView: some View {
+        VStack(spacing: .large) {
+            Spacer()
+            
+            urlTextField
+            
+            primaryButton
+            
+            Spacer()
+        }
+        .padding(.large)
+    }
+    
+    private var urlTextField: some View {
+        TextField("Enter URL", text: $url)
+            .textInputAutocapitalization(.never)
+            .padding(.medium)
+            .border(theme.secondaryBackgroundColor, width: .xxSmall)
+            .clipShape(RoundedRectangle(cornerRadius: .small))
+    }
+    
+    private var primaryButton: some View {
+        Button {
+            Task {
+                guard !isPrimaryButtonLoading else {
+                    return
+                }
+                
+                isPrimaryButtonLoading = true
+                
+                do {
+                    asset = try await generateAsset(url)
+                    
+                    url = ""
+                } catch {
+                    didFailToGenerate = true
+                }
+                
+                isPrimaryButtonLoading = false
+            }
+        } label: {
+            ZStack {
+                Text("Generate asset")
+                    .opacity(isPrimaryButtonLoading ? 0 : 1)
+                
+                if isPrimaryButtonLoading {
+                    ProgressView()
+                        .progressViewStyle(.circular)
+                        .tint(.white)
+                }
+            }
+        }
+        .primaryStyle($isPrimaryButtonLoading)
+        .disabled(url.isEmpty)
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    CreateAutomaticallyView(
+        selectedTab: .constant(.home),
+        generateAsset: { url in
+            return LearningAsset.mockLearningAssets.first!
+        }
+    )
+}
